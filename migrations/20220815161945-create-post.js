6c4db27263c07ee539189265a04b7bbbb4d90170
Cam <github@lekienhoanh.dev>
2022-08-15 23:24:15 +0700

    update post model

--- migrations/20220815161945-create-post.js
+++ migrations/20220815161945-create-post.js
@@ -1,39 +1,33 @@
-'use strict';
+"use strict";
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Posts', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      title: {
-        type: Sequelize.STRING
-      },
-      slug: {
-        type: Sequelize.STRING
-      },
-      category_id: {
-        type: Sequelize.INTEGER
-      },
-      created_at: {
-        type: Sequelize.STRING
-      },
-      updated_at: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Posts');
-  }
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable("Posts", {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			title: {
+				type: Sequelize.STRING,
+			},
+			slug: {
+				type: Sequelize.STRING,
+			},
+			category_id: {
+				type: Sequelize.INTEGER,
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable("Posts");
+	},
 };
